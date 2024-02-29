e97d60d9274d6a9b7e35155be0180d88dac0254a
Carlos Walter Pacheco Lora <cwpachecol@usfx.bo>
2024-02-29 13:04:42 -0400

    Agregar archivos de proyecto.

---
+++ Source/Galaga_USFX_L02/Galaga_USFX_L02GameMode.h
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Galaga_USFX_L02GameMode.generated.h"
+
+class ANaveEnemiga;
+
+UCLASS(MinimalAPI)
+class AGalaga_USFX_L02GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGalaga_USFX_L02GameMode();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	ANaveEnemiga* NaveEnemiga01;
+};
+
+
+
